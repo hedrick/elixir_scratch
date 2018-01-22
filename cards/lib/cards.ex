6520215291b1dbbe603f6f7800f390895b66a2e4
Scott Hedrick <me@scottshedrick.com>
2018-01-22 16:08:57 -0600

    creating a deck of cards with list comprehensions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,31 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  
+  def create_deck do
+    values = [
+      "A", "2", "3", "4",
+      "5", "6", "7", "8", 
+      "9", "10", "11", "12", 
+      "13"
+    ]
+    suits = [
+      "Hearts", 
+      "Spades", 
+      "Clubs", 
+      "Diamonds"
+    ]
 
-  @doc """
-  Hello world.
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
 
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
 end
