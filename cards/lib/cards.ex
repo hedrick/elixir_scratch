2208582f199b7ec28f79b40cdacbf6629c732bf0
Scott Hedrick <me@scottshedrick.com>
2018-01-22 16:41:43 -0600

    added file read/write functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,23 +1,13 @@
-defmodule Cards do
-  
+defmodule Cards do  
   def create_deck do
     values = [
-      "A", "2", "3", "4",
-      "5", "6", "7", "8", 
-      "9", "10", "11", "12", 
-      "13"
+      "A", "2", "3", "4", "5", "6", "7",
+      "8", "9", "10", "J", "Q", "K"
     ]
-    suits = [
-      "Hearts", 
-      "Spades", 
-      "Clubs", 
-      "Diamonds"
-    ]
-
+    suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
     for suit <- suits, value <- values do
         "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
@@ -28,4 +18,22 @@ defmodule Cards do
     Enum.member?(deck, hand)
   end
 
+  def deal(deck, hand_size) do
+    hand = Enum.split(deck, hand_size)
+    elem(hand, 0)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
+  end
 end
