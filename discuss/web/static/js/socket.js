c6f5d7925e26614a8d0f8f5131e2aaa530437871
Scott Hedrick <me@scottshedrick.com>
2018-01-29 13:40:26 -0600

    comments now updating by topic live

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -8,6 +8,8 @@ const createSocket = (topicId) => {
   channel.join()
     .receive("ok", resp => { renderComments(resp.comments) })
     .receive("error", resp => { console.log("Unable to join", resp) })
+  
+  channel.on(`comments:${topicId}:new`, renderComment)
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
@@ -18,14 +20,24 @@ const createSocket = (topicId) => {
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment => {
-    return `
-      <li class="collection-item">
-        ${comment.content}
-      </li>
-    `;
+    return commentTemplate(comment)
   });
 
   document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  return `
+    <li class="collection-item">
+      ${comment.content}
+    </li>
+  `
 }
 
 window.createSocket = createSocket;
