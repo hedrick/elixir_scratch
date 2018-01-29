5f017fd385681a51100645a8eb9b5a30f6238c4a
Scott Hedrick <me@scottshedrick.com>
2018-01-29 14:14:02 -0600

    comments now have a user_id assigned to them and are being verified in sockets

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -1,13 +1,17 @@
 import {Socket} from "phoenix"
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", { params: { token: window.userToken } })
 
 socket.connect()
 
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { renderComments(resp.comments) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => { 
+      renderComments(resp.comments) 
+    })
+    .receive("error", resp => { 
+      console.log("Unable to join", resp) 
+    })
   
   channel.on(`comments:${topicId}:new`, renderComment)
 
